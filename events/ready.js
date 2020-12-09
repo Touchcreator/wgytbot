606bb6347d6782eb8ac7e338d2cf1f384a7002d8
William (Wgyt) <wgyt@usa.com>
2020-12-09 17:24:33 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 module.exports = (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setUsername('wasteof.bot');
+	client.user.setUsername('wgytbot');
     client.user.setPresence({
         status: "online",  //You can show online, idle....
         game: {
-						url:"https://bot.wgyt.tk/",
+	    url:"https://bot.wgyt.tk/",
             name: "commands.",  //The message shown
             type: "LISTENING" //PLAYING: WATCHING: LISTENING: STREAMING:
         }
