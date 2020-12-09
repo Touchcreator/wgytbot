42c6d4ff0717014da28a3b412f3ce1dd134188f4
William (Wgyt) <github@wgyt.tk>
2020-12-09 15:44:21 +0000

    fixes3

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setPresence({
-			status:"none",
+			status:none,
         game: {
             name: "commands.",  //The message shown
             type: "LISTENING", //PLAYING: WATCHING: LISTENING: STREAMING:
